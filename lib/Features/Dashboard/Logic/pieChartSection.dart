5db2750cdb29ea4edc12767d9917b6d5451db042
YASSIR006 <nasriyasser006@gmail.com>
2023-09-10 13:26:44 +0100

    UPGRADED VERSION

--- lib/Features/Dashboard/Logic/pieChartSection.dart
+++ lib/Features/Dashboard/Logic/pieChartSection.dart
@@ -7,46 +7,57 @@ import '../Services/formatNumber.dart';
 
 List<PieChartSectionData> getSections(int touchedIndex, List<Data> data) {
   final double totalValue = data.map((d) => d.percent).reduce((a, b) => a + b);
-  final double threshold = 0.05; 
+  final double threshold = 0.05;
 
   List<PieChartSectionData> sections = [];
-
   double otherValue = 0.0;
-  Color otherColor = Colors.grey; 
+
+
 
   for (int index = 0; index < data.length; index++) {
     final dataPoint = data[index];
     final percent = dataPoint.percent / totalValue;
 
-    if (percent >= threshold) {
-      final isTouched = index == touchedIndex;
-      final double fontSize = isTouched ? 15 : 10;
-      final double radius = isTouched ? 60 : 40;
 
-      final value = PieChartSectionData(
-        color: isTouched ? dataPoint.colorOpac : dataPoint.color,
-        value: dataPoint.percent,
-        title: '${formatNumber(dataPoint.percent)}',
-        titlePositionPercentageOffset: 0.55,
-        radius: radius,
-        titleStyle: TextStyle(
-          fontSize: fontSize,
-          fontWeight: FontWeight.bold,
-          color: const Color(0xffffffff),
-        ),
-      );
+    if(percent < threshold){
+    
+      otherValue += dataPoint.percent;
 
-      sections.add(value);
-    } else {
-      otherValue += dataPoint.percent; 
+
     }
-  }
 
+
+    final isTouched = index == touchedIndex;
+    final double fontSize = isTouched ? 15 : 10;
+    final double radius = isTouched ? 60 : 40;
+
+    final value = PieChartSectionData(
+      color: isTouched ? dataPoint.colorOpac : dataPoint.color,
+      value: dataPoint.percent,
+      title: percent >= threshold
+          ? '${formatNumber(dataPoint.percent)}'
+          : '',
+
+
+      titlePositionPercentageOffset: 0.55,
+      radius: radius,
+      titleStyle: TextStyle(
+        fontSize: fontSize,
+        fontWeight: FontWeight.bold,
+        // color: const Color(0xff000000),
+        color: const Color(0xffffffff),
+      ),
+    );
+
+    sections.add(value);
+
+
+  }
   if (otherValue > 0) {
     sections.add(PieChartSectionData(
-      color: otherColor,
+      color: Color.fromRGBO(0, 0, 0, 0),
       value: otherValue,
-      title: '${formatNumber(otherValue)}', 
+      title: '${formatNumber(otherValue)}',
       titlePositionPercentageOffset: 0.55,
       radius: 40,
       titleStyle: TextStyle(
@@ -56,6 +67,7 @@ List<PieChartSectionData> getSections(int touchedIndex, List<Data> data) {
       ),
     ));
   }
+
 
   return sections;
 }
